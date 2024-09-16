c0d0754bf5e456c44bf522f7ca117546e9dd81a1
Rahullchaudharyy <rahullchaudharryy@gmail.com>
2024-09-16 19:07:36 +0530

    "Rtk"

--- Redux-To-Do/src/components/Todo.jsx
+++ Redux-To-Do/src/components/Todo.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+
+import { useDispatch, useSelector } from 'react-redux';
+import { removeTodo } from '../features/todo/todoSlice';
 
 const Todo = () => {
-  const todos =  useSelector(state=>state.todos)
-  const dispatch = useDispatch()
-  return (
-    <div>{
-        todos.map((value)=>(
-            <li key={value.id}>{value.text} <button onClick={()=>{
-                dispatch(removeTodo(value.id))
-            }}>X</button></li>
+    const todos = useSelector(state => state.todos);
+    const dispatch = useDispatch();
 
-        ))}</div>
-  )
-}
+    return (
+        <div className="max-w-md mx-auto mt-8 p-4 bg-white shadow-lg rounded-lg">
+            <h2 className="text-2xl font-semibold text-center mb-4">Todo List</h2>
+            <ul className="space-y-4">
+                {todos.map((value) => (
+                    <li
+                        key={value.id}
+                        className="flex justify-between items-center bg-gray-100 p-3 rounded-md shadow-sm"
+                    >
+                        <span className="text-lg">{value.text}</span>
+                        <button
+                            onClick={() => dispatch(removeTodo(value.id))}
+                            className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600 transition duration-200"
+                        >
+                            X
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
-export default Todo
+export default Todo;
